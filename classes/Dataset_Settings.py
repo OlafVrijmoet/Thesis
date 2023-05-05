51d7a1468ce76a185abbe6ecab10516cd5443ef1
OlafVrijmoet <60693445+OlafVrijmoet@users.noreply.github.com>
2023-05-05 15:56:37 +0530

    add validation split

--- classes/Dataset_Settings.py
+++ classes/Dataset_Settings.py
@@ -24,10 +24,15 @@ class Dataset_Settings:
     
     def split_datasets(self, seed, x_column_name, y_column_name):
 
+        # get values
         x = self.df[x_column_name].values.reshape(-1, 1)
         y = self.df[y_column_name]
 
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.2, random_state=seed)
+        # Split the DataFrame into train (70%) and the remaining data (30%)
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.3, random_state=seed)
+
+        # Split the remaining data further into test (20% of the original dataset) and validation (10% of the original dataset) sets
+        self.x_test, self.x_validation, self.y_test, self.y_validation = train_test_split(self.y_train, self.y_test, test_size=1/3, random_state=seed)
 
     def __getitem__(self, key):
         return getattr(self, key)
