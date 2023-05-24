c328a91cc5ec5afac7923c2692263dc163aab095
OlafVrijmoet <60693445+OlafVrijmoet@users.noreply.github.com>
2023-05-24 14:47:54 +0200

    update Dataset_Settings #40

--- classes/Dataset_Settings.py
+++ classes/Dataset_Settings.py
@@ -34,28 +34,6 @@ class Dataset_Settings:
 
         self.name_required_dataset = name_required_dataset
 
-        # delete
-        self.train_df = train_df
-        self.test_df = test_df
-        self.validation_df = validation_df
-        
-        x_train = x_train
-        x_test = x_test
-        x_validation = x_validation
-
-        y_train = y_train
-        y_test = y_test
-        y_validation = y_validation
-    
-    # delete
-    def split_datasets(self, seed):
-
-        # Split the DataFrame into train (70%) and the remaining data (30%)
-        self.train_df, temp_df = train_test_split(self.df, test_size=0.3, random_state=seed)
-
-        # Split the remaining data further into test (20% of the original dataset) and validation (10% of the original dataset) sets
-        self.test_df, self.validation_df = train_test_split(temp_df, test_size=1/3, random_state=seed)
-
     def __getitem__(self, key):
         return getattr(self, key)
 
